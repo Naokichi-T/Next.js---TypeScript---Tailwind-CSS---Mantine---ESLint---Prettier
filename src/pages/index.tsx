71b2bf9ca8e3c4d06f3c82b38f783bb879c1ddca
naokichi <naokichi.tany@gmail.com>
2022-04-10 16:28:41 +0900

    Add Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next'
+import { Button } from '@mantine/core'
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button className="mt-3">Button</Button>
+    </>
+  )
 }
 
 export default Home
